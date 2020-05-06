ddf9098d76c2895be67992847b62000d7c9b68cc
digaodev <rodrigo.rb.dev@gmail.com>
2020-05-06 18:40:14 -0300

    initial commit

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,38 @@
+import { isEqual } from 'date-fns';
+
+import Appointment from '../models/Appointment';
+
+interface CreateAppointmentDTO {
+  provider: string;
+  date: Date;
+}
+
+class AppointmentsRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  public all(): Appointment[] {
+    return this.appointments;
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const appointmentFound = this.appointments.find(appointment =>
+      isEqual(appointment.date, date),
+    );
+
+    return appointmentFound || null;
+  }
+
+  public create({ provider, date }: CreateAppointmentDTO): Appointment {
+    const newAppointment = new Appointment({ provider, date });
+
+    this.appointments.push(newAppointment);
+
+    return newAppointment;
+  }
+}
+
+export default AppointmentsRepository;
