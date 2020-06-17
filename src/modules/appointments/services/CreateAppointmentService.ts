4c2bd8c387470c375b951e61ed8b425ceb6e35ba
digaodev <rodrigo.rb.dev@gmail.com>
2020-06-17 15:59:26 -0300

    refactor: adapt the Appointments module to better conform to SOLID principles.

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,26 +1,26 @@
 import { startOfHour } from 'date-fns';
-import { getCustomRepository } from 'typeorm';
 
 // Models
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
 // Repositories
-import AppointmentsRepository from '@modules/appointments/repositories/AppointmentsRepository';
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 
 // Utils
 import AppError from '@shared/errors/AppError';
 
-interface Request {
+interface IRequest {
   provider_id: string;
   date: Date;
 }
 
 class CreateAppointmentService {
-  public async execute({ provider_id, date }: Request): Promise<Appointment> {
-    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  constructor(private appointmentsRepository: IAppointmentsRepository) {}
+
+  public async execute({ provider_id, date }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
-    const hasAppointmentOnSameDate = await appointmentsRepository.findByDate(
+    const hasAppointmentOnSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
     );
 
@@ -28,12 +28,10 @@ class CreateAppointmentService {
       throw new AppError('This appointment is already booked');
     }
 
-    const newAppointment = appointmentsRepository.create({
+    const newAppointment = await this.appointmentsRepository.create({
       provider_id,
       date: appointmentDate,
     });
-
-    await appointmentsRepository.save(newAppointment);
 
     return newAppointment;
   }
